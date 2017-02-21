e86291b6c29f06196c2436f336ce30f6523cfcf1
sugam11 <[sugam110795@gmail.com]>
2017-02-21 21:14:29 +0530

    finished execvp call

--- driver.c
+++ driver.c
@@ -6,45 +6,46 @@
 void pr_exit (int status);
 
 int main(){
-    char command[10][10],c;
-    char command_arg[10][100];
+    char command[10][10];
     int command_counter = 0;
     pid_t pid[50];
     pid_t id;
     int process_no = 0,status;
     const char delim[] = " \t\r\n\v\f";
     while(1){
-        scanf("%s",command[command_counter]);
-        c = getc(stdin);
         
-        gets(command_arg[command_counter]);
-        printf("%s\n",command_arg[command_counter]);
-        //Command tokenization begins
+        gets(command[command_counter]);
         
-//        char *command_tokens[100];
-//        int i = 0;
-//        command_tokens[i]  = strtok(command,delim);
-//        while(command_tokens[i] != NULL){
-//            //printf("%s\n",command_tokens[i]);
-//            i++;
-//            command_tokens[i]=strtok(NULL,delim);
-//            
-//        }
-        //Command tokenization ends
+        //printf("%s\n",command[command_counter]);
         
+        
+//Command tokenization begins
+        
+        char *command_tokens[100];
+        int i = 0;
+        command_tokens[i]  = strtok(command,delim);
+        while(command_tokens[i] != NULL){
+            i++;
+            command_tokens[i]=strtok(NULL,delim);
+            
+        }
+//Command tokenization ends
+        
+//Execvp call begins
         if ((pid[process_no] = fork ()) < 0)
             perror ("fork error");
         else if (pid[process_no] == 0)
         {
             printf ("Executing %s with argument %s \n", command[command_counter],command_arg[command_counter]);
-            execvp (command[command_counter],&command_arg[command_counter]);
-            perror ("error in execlp");
+            execvp(command_tokens[0],command_tokens);
+            perror ("error in execvp");
         }
         else if (pid[process_no] > 0)
         {
             wait (&status);
             pr_exit (status);
         }
+//Execvp call ends
 
     }
 }
