4aa4e45961b4b281359ff176c8f278edb7605d51
sugam11 <[sugam110795@gmail.com]>
2017-02-21 15:16:00 +0530

    Added first driver file

---
+++ driver.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+int main(){
+    char command[10][100];
+    int command_counter = 0;
+    while(1){
+        scanf("%s",command[command_counter]);
+        
+    }
+}
