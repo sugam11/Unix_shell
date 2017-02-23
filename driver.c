58e9ff76d46388c3b20a900c64425682b3c64eb8
sugam11 <[sugam110795@gmail.com]>
2017-02-23 19:03:13 +0530

    Parsing complete and redirection op complete

--- driver.c
+++ driver.c
@@ -7,140 +7,142 @@
 #include <fcntl.h>
 
 void pr_exit (int status,int process_id);
-
+void child (char * file_name, int op,char ** command);
 int main(){
-    char command[10][10];
+    
     char *copy;
-    int command_counter = 0;
+    int counter = 0;
     pid_t pid[50];
     pid_t id;
     int process_no = 0,status;
     const char delim[] = " \t\r\n\v\f";
     while(1){
-        int j = 0;
-        gets(command[command_counter]);
-        //printf("%s\n%s\n",command[command_counter],copy);
+        char input[100];
+        gets(input);
         
 //Detecting operators
-        short op1,op2,op3,op4,op5,op6 = 0;                  // '>','>>','<','|','||','|||' operator
-        for (j = 0; j < strlen(command[command_counter]); j++) {
-            if(command[command_counter][j] == '<'){
-                op3 = 1;
-                command[command_counter][j] = ' ';
-            }
-            else if(command[command_counter][j] == '>'){
-                op1 = 1;
-                command[command_counter][j] = ' ';
-                //printf("%d",flag);
-                if(command[command_counter][j+1] != '\0' && command[command_counter][j+1] == '>'){
-                    op2 = 1;
-                    op1 = 0;
-                    j++;
-                    command[command_counter][j] = ' ';
-                    //printf("%d",flag);
+        char op[10][4];                 //Operator 2-d array in order of appearance
+        char com[11][50];                   //Commands separated by their operators
+        int i = 0,j = 0,k = 0,start = 0,end = 0;        //k is for com counter,j is for delim counter
+        while(input[i] != '\0')
+        {
+            if(input[i] == '<' || input[i] == '>' || input[i] == '|' || input[i]==','){
+                end = i;
+                if(input[i+1] != '\0' && input[i+1] == '>'){
+                    strcpy(op[j++],">>");
+                    i++;
                 }
-            }
-            else if(command[command_counter][j] == '|'){
-                op4 = 1;
-                command[command_counter][j] = ' ';
-                //printf("%d",flag);
-                if(command[command_counter][j+1] != '\0' && command[command_counter][j+1] == '|'){
-                    // Add error detection here if two files separated by comma are not written
-                    op5 = 1;
-                    op4 = 0;
-                    j++;
-                    command[command_counter][j] = ' ';
-                    //printf("%d",flag);
+                else if(input[i+1] != '\0' && input[i+1] == '|'){
+                    if(input[i+2] != '\0' && input[i+2]=='|'){
+                        strcpy(op[j++],"|||");
+                        i += 2;
+                    }
+                    else{
+                        strcpy(op[j++],"||");
+                        i++;
+                    }
                 }
-                else if(command[command_counter][j+1] != '\0' && command[command_counter][j+1] == '|'){
-                    op6 = 1;
-                    op5 = 0;
-                    j++;
-                    command[command_counter][j] = ' ';
-                    //printf("%d",flag);
+                else{
+                    op[j++][0] = input[i];
+                    op[j++][1] = '\0';
                 }
+        //Because strncpy wasnt working so manually copying all commands in com 2-d array
+                int a,b;
+                for (a = start, b = 0; a < end; a++,b++) {
+                    com[k][b] = input[a];
+                    //printf("%c\n",com[k][b]);
+                }
+                k++;
+                start = i + 1;
             }
+            i++;
+        }
+    //The single command, last command to be copied too !!
+        int a,b;
+        //printf("%d %d %d\n",end,start,strlen(input));
+        for (a = start, b = 0; a < strlen(input); a++,b++) {
+            com[k][b] = input[a];
+            //printf("This is it%c\n",com[k][b]);
         }
         
-        //printf("%d %d %d %d %d %d ",op1,op2,op3,op4,op5,op6);
+//        for(i = 0; i < j; i++){
+//            printf("%s\n",op[i]);
+//            printf("%s\n",com[i]);
+//        }
         
 //Operator detection ends
+        int op_num = j,op_counter = 0,com_num = k,com_counter;
         
-//Command tokenization begins
-        char *command_delim[100];
-        int i = 0;
-        char *command_tokens[100];
-        command_tokens[i]  = strtok(command[command_counter],delim);
-        while(command_tokens[i] != NULL){
-            //printf("%s\n",command_tokens[i]);
-            i++;
-            command_tokens[i]=strtok(NULL,delim);
-        }
-//Command tokenization ends
-        
-        
-        int fd1;
-        
-        
-        if(flag != 0){
+        if(op_num > 0){
+            while (op_counter < op_num) {
+                //      Command tokenization begins
+                int token_no = 0;
+                char *command_tokens[100];
+                command_tokens[token_no]  = strtok(com[com_counter],delim);
+                while(command_tokens[token_no] != NULL){
+                    //printf("%s\n",command_tokens[i]);
+                    token_no++;
+                    command_tokens[token_no]=strtok(NULL,delim);
+                }
+                
+                //      Command tokenization ends
+
+                char *file_name;
+                file_name = strtok(com[com_counter + 1],delim);
+                if(strcmp(op[op_counter],">") == 0){
+                    child(file_name,1,command_tokens); // Execvp call
+                }
+                else if(strcmp(op[op_counter],">>") == 0){
+                    child(file_name,2,command_tokens);  //Execvp call
+                }
+                else if(strcmp(op[op_counter],"<") == 0){
+                    child(file_name,3,command_tokens);  //Execvp call
+                }
+                else if(strcmp(op[op_counter],"|") == 0){
+                    
+                }
+                op_counter++;
+            }
             
-            char file_name[100];
-            i = 1;
-            if(strcmp(command_delim[i],">") == 0){
-                i++;
-            }
-            printf("I am :%s\n",command_delim[i]);
-            strcpy(file_name,command_delim[i]);
-            if(flag == 2){
-                fd1 = open(file_name,O_CREAT|O_WRONLY,S_IRWXU);
-                printf("FD is  :: %d\n",fd1);
-            }
-            else if(flag == 1){
-                fd1= open(file_name,O_RDONLY);
-            }
-            else if(flag == 3){
-                close(1);
-                fd1 = open(file_name,O_APPEND|O_WRONLY|O_CREAT,S_IRWXU);
-            }
         }
-
-
         
 //Execvp call begins
-        if ((pid[process_no] = fork ()) < 0)
-            perror ("fork error");
-        else if (pid[process_no] == 0)
-        {
-            if(flag != 0){
-                if(flag == 2){
-                    close(1);
-                    dup(fd1);
-                    printf("FD is  :: %d\n",fd1);
-                }
-                else if(flag == 1){
-                    close(0);
-                    dup(fd1);
-                }
-                else if(flag == 3){
-                    printf("flag=3");
-                    close(1);
-                    dup(fd1);
-                }
-            }
-            printf ("Executing %s\n", command_tokens[0]);
-            execvp(command_tokens[0],command_tokens);
-            perror ("error in execvp");
-        }
-        else if (pid[process_no] > 0)
-        {
-            id = wait (&status);
-            pr_exit (status,id);
-        }
-//Execvp call ends
+
 
     }
 }
 
+void child(char *file_name,int op, char ** command_tokens){
+    pid_t pid,id;
+    int status,fd1;
+    if ((pid = fork ()) < 0)
+        perror ("fork error");
+    else if (pid == 0)
+        {
+            printf ("Executing %s\n", command_tokens[0]);
+            if(op == 1){
+            close(1);
+            fd1 = open(file_name,O_CREAT|O_WRONLY,S_IRWXU);
+            }
+            else if (op == 2){
+                close(1);
+                fd1 = open(file_name,O_CREAT|O_APPEND|O_WRONLY,S_IRWXU);
+            }
+            else if (op == 3){
+                close(0);
+                fd1 = open(file_name,O_RDONLY);
+            }
+            execvp(command_tokens[0],command_tokens);
+            perror ("error in execvp");
+        }
+    else if (pid > 0)
+        {
+            id = wait (&status);
+            pr_exit (status,id);
+        }
+
+}
+//Execvp call ends
 
 void pr_exit (int status,int process_id)
 {
